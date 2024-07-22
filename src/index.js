25ffff6ff88947d3293aec7e15bc7e34da5be3e5
Bhuvan <bhuvanshyam2022@gmail.com>
2024-07-22 22:45:50 +0530

    added repository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
 const bodyParser = require("body-parser");
+const CityRepository = require('./repository/city-repository')
 
 const setupAndStartServer = async () => {
   const app = express();
@@ -10,6 +11,8 @@ const setupAndStartServer = async () => {
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    const repo = new CityRepository();
+    repo.createCity({name:'Bangalore'})
   });
 };
 setupAndStartServer();
