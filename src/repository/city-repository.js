ff06fb4f1e7e3df3db5cc196062befc35405d477
Bhuvan <bhuvanshyam2022@gmail.com>
2024-07-22 23:09:59 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,6 +6,7 @@ class CityRepository {
       const city = await City.create({ name });
       return city;
     } catch (error) {
+      console.log("Something went wrong!!!");
       throw { error };
     }
   }
@@ -13,16 +14,29 @@ class CityRepository {
   async deleteCity({ cityid }) {
     try {
       await City.destroy({ where: { id: cityid } });
+      return true;
     } catch (error) {
+      console.log("Something went wrong!!!");
       throw { error };
     }
   }
 
-  async updateCity({ cityid }) {
+  async updateCity({ cityid, data }) {
     try {
-      const city = await City.findOne({ where: { id: cityid } });
+      const city = await City.update(data, { where: { id: cityid } });
       return city;
     } catch (error) {
+      console.log("Something went wrong!!!");
+      throw { error };
+    }
+  }
+
+  async getCity({ cityid }) {
+    try {
+      const city = await City.findByPk(cityid);
+      return city;
+    } catch (error) {
+      console.log("Something went wrong!!!");
       throw { error };
     }
   }
